9f45c3707e9d6c94614db5b4540e4345687f3606
Alireza-Mak <ics24-041@camosun.bc.ca>
2026-01-09 22:14:34 -0800

    Initial UI

--- MemoryGame2026/ContentView.swift
+++ MemoryGame2026/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var img = ["sun.max","cloud.sun","cloud.rain"]
+    @State private var stage:Int = 0
+    @State private var selectedPhoto: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+        HStack {
+            Image(systemName: "arrowtriangle.left")
+                .resizable()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    stage = stage == 0 ? img.count - 1 : stage - 1
+                }
+
+            Image(systemName: img[stage])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.black)
+                .padding(25)
+            
+            Image(systemName: "arrowtriangle.right")
+                .resizable()
+                .foregroundStyle(.tint)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                .onTapGesture {
+                    stage = (stage + 1) % img.count
+                }
         }
         .padding()
+        HStack{
+           Text(img[stage])
+        }
     }
 }
 
