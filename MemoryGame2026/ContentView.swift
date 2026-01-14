4db4f8aeaba3a0ac4db71db49f21a1b17007aa0d
Alireza-Mak <ics24-041@camosun.bc.ca>
2026-01-13 22:27:20 -0800

    Added stepper and create a child view

--- MemoryGame2026/ContentView.swift
+++ MemoryGame2026/ContentView.swift
@@ -8,48 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let images = ["sun.max", "cloud.sun", "cloud.rain"]
+    @State private var images = ["sun.max", "cloud.sun", "cloud.rain"]
     @State private var index: Int = 0
+    @State private var step: Int = 7
+    @State private var bouns: Bool = true
 
     var body: some View {
-        HStack(spacing: 16) {
-            Image(systemName: "arrowtriangle.left")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 44, height: 44)
-                .foregroundStyle(.tint)
-                .accessibilityLabel("Previous image")
-                .onTapGesture { showPrevious() }
-
-            Image(systemName: images[index])
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundStyle(.primary)
-                .frame(maxWidth: 200, maxHeight: 200)
-                .padding(24)
-                .accessibilityLabel(images[index])
-
-            Image(systemName: "arrowtriangle.right")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 44, height: 44)
-                .foregroundStyle(.tint)
-                .accessibilityLabel("Next image")
-                .onTapGesture { showNext() }
+        VStack(spacing: 20){
+            ImagePickerView(
+                index: $index,
+                images: $images
+            )
+            Stepper(value:$step, in:5...10){Text("\(step) Rows/Cols")}
+            Toggle("Bouns", isOn: $bouns)
         }
-        .padding()
-
-        Text(images[index])
-            .font(.headline)
-            .padding(.bottom)
-    }
-
-    private func showPrevious() {
-        index = index == 0 ? images.count - 1 : index - 1
-    }
-
-    private func showNext() {
-        index = (index + 1) % images.count
+        .padding(30)
     }
 }
 
---
+++ MemoryGame2026/ImagePickerView.swift
@@ -0,0 +1,50 @@
+//
+//  ImagePickerView.swift
+//  MemoryGame2026
+//
+//  Created by  on 2026-01-13.
+//
+import SwiftUI
+
+struct ImagePickerView: View {
+   @Binding var index: Int
+    @Binding var images: [String]
+    var body: some View {
+        HStack() {
+            Image(systemName: "arrowtriangle.left")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 44, height: 44)
+                .foregroundStyle(.tint)
+                .accessibilityLabel("Previous image")
+                .onTapGesture { showPrevious() }
+
+            Image(systemName: images[index])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.primary)
+                .frame(maxWidth: 100, maxHeight: 100)
+                .padding(.vertical, 20)
+                .padding(.horizontal, 70)
+                .accessibilityLabel(images[index])
+
+            Image(systemName: "arrowtriangle.right")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 44, height: 44)
+                .foregroundStyle(.tint)
+                .accessibilityLabel("Next image")
+                .onTapGesture { showNext() }
+        }
+        .padding()
+    }
+    
+    private func showPrevious() {
+        index = index == 0 ? images.count - 1 : index - 1
+    }
+
+    private func showNext() {
+        index = (index + 1) % images.count
+    }
+}
+
